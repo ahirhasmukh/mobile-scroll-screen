d8c45aa1a77f408768962651938ac952ebd7b9ee
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2018-08-22 17:25:39 +0530

    animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {animate, style, transition, trigger} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   animations: [
     trigger('slideInOut', [
       transition(':enter', [
